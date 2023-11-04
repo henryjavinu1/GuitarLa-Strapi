8458ec0b6b445ed82c328006fff9ab59faf90c41
henryjavinu1 <henryjavinu1@gmail.com>
2023-11-03 22:12:01 -0600

    Subiendo el proyecto completo de Stripi

---
+++ config/middlewares.js
@@ -0,0 +1,31 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http://localhost'],  // Agregar http://localhost
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors', // Mantén esta línea si ya la tienes
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
